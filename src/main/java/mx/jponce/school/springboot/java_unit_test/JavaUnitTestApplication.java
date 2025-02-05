e11a841dabaf6279ccf6d3d8f6823515af4ba536
11112487 <jesus.f.ponce@autozone.com>
2025-02-05 15:48:02 -0600

    first commit

---
+++ src/main/java/mx/jponce/school/springboot/java_unit_test/JavaUnitTestApplication.java
@@ -0,0 +1,13 @@
+package mx.jponce.school.springboot.java_unit_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaUnitTestApplication.class, args);
+	}
+
+}
